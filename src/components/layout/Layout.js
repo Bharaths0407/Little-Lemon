424cf8d2875d68452e3ff2e305bc96a33333395e
Bharath S <bharaths0407@gmail.com>
2023-10-16 19:58:55 +0530

    Layouts done

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,14 @@
-import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
